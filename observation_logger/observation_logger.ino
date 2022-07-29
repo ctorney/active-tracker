c0df128a651501cc00b350cb3a739d4127e87544
Colin Torney <colin.j.torney@gmail.com>
2022-07-29 13:38:26 +0100

    logging to sd card with gps time

--- observation_logger/observation_logger.ino
+++ observation_logger/observation_logger.ino
@@ -23,6 +23,8 @@ String behaviours[] = {
 Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 #include <SPI.h>
+#include <SD.h>
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -38,6 +40,18 @@ Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPi
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+#define GPS_I2C_ADDRESS 0x10
+
+#include <Adafruit_GPS.h>
+Adafruit_GPS GPS(&Wire);
+
+const int chipSelect = SDCARD_SS_PIN;
+
+
+
 
 unsigned long updateTime = 1000;
 unsigned long lastUpdate = 0;
@@ -62,10 +76,34 @@ void setup() {
   // Clear the buffer
   display.clearDisplay();
 
+  Serial.print("Initializing SD card...");
+//*
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+
+if (!GPS.begin(GPS_I2C_ADDRESS)) 
+  {
+    Serial.println("ERROR: GPS");
+        while (true);
+  }
+
+  
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  
 
 }
 
 void loop() {
+
+  char c = GPS.read();
+  if (GPS.newNMEAreceived()) GPS.parse(GPS.lastNMEA());
+  
   
   customKeypad.tick();
 
@@ -79,56 +117,113 @@ void loop() {
   while(customKeypad.available()){
     keypadEvent e = customKeypad.read();
     
-    Serial.print((uint8_t)e.bit.COL);
+    
     if(e.bit.EVENT == KEY_JUST_PRESSED){
-      Serial.println(" pressed");
+      
       behaviour = (int)e.bit.COL;
       update_display();
+      save_behaviour();
     }
-    else if(e.bit.EVENT == KEY_JUST_RELEASED) Serial.println(" released");
+    
     
   }
 
   delay(10);
 }
 
-void testdrawstyles(int behaviour) {
-  display.clearDisplay();
+void save_behaviour()
+{
 
-  
-  
-  display.display();
+
+  String gps_year = String("20" + String(GPS.year, DEC));
+  String gps_month;
+  String gps_day;
+  if (GPS.month<10)
+    gps_month =  String("0" +String(GPS.month, DEC));
+  else
+    gps_month =  String(GPS.month, DEC);
+
+  if (GPS.day<10)
+    gps_day =  String("0" +String(GPS.day, DEC));
+  else
+    gps_day =  String(GPS.day, DEC);
+
+  String filename = String("AL-" + gps_day + "-" + gps_month + ".csv");  
+
+  File dataFile = SD.open(filename, FILE_WRITE);
+
+  if (dataFile) 
+  {
+
+    Serial.print("Writing to ");
+    Serial.println(filename);
+
+    if (GPS.hour < 10) { dataFile.print('0'); }
+    dataFile.print(GPS.hour, DEC); dataFile.print(':');
+    if (GPS.minute < 10) { dataFile.print('0'); }
+    dataFile.print(GPS.minute, DEC); dataFile.print(':');
+    if (GPS.seconds < 10) { dataFile.print('0'); }
+    dataFile.print(GPS.seconds, DEC); dataFile.print('.');
+    if (GPS.milliseconds < 10) {
+      dataFile.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      dataFile.print("0");
+    }
+    dataFile.print(GPS.milliseconds);
+    dataFile.print(",");
+    dataFile.println(behaviour);
+    dataFile.close();
+    
+    Serial.println("done.");
+
+  } else {
+
+    // if the file didn't open, print an error:
+
+    Serial.println("error opening file");
+
+  }
+
   
 }
 
-void update_display() {
+void update_display() 
+{
   display.clearDisplay();
-  unsigned long currentMillis = millis();  
-
   display.setTextSize(2);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,16);             // Start at top-left corner
 
-  //display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+
   if (behaviour<0)
   {
-  
-  display.println("READY");
+    if (GPS.fix){
+      display.println("READY");
+    }
+    else{
+      display.println("WAITING");
+      display.println("FOR GPS");
+    }
   }
   else
   {
-display.print(behaviour+1);
-  display.print(":");
-  display.println(behaviours[behaviour]);
-    
+    display.print(behaviour+1);
+    display.print(":");
+    display.println(behaviours[behaviour]);
   }
 
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,50);             // Start at top-left corner
 
-  display.println(currentMillis);
 
+  if (GPS.hour < 10) { display.print('0'); }
+  display.print(GPS.hour, DEC); display.print(':');
+  if (GPS.minute < 10) { display.print('0'); }
+  display.print(GPS.minute, DEC); display.print(':');
+  if (GPS.seconds < 10) { display.print('0'); }
+  display.print(GPS.seconds, DEC); 
+  
   
   display.display();
   
