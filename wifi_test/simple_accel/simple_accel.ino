b504886ed97e036b3ac9c63331ec24d8b3a8868d
Colin Torney <colin.j.torney@gmail.com>
2022-10-07 08:31:34 +0100

    simple broadcast of accelerometer over wifi - no sleep mode

--- wifi_test/simple_accel/simple_accel.ino
+++ wifi_test/simple_accel/simple_accel.ino
@@ -20,6 +20,18 @@ sensors_event_t gyro;
 sensors_event_t temp;
 
 
+#include <imuFilter.h>
+
+constexpr float GAIN = 0.75;     // Fusion gain determines response of heading correction with respect to gravity.
+imuFilter <&GAIN> filter;
+
+float acc_bias[3] = {0.0f, 0.0f, SENSORS_GRAVITY_EARTH};
+float gyro_bias[3] = {0.0f, 0.0f, 0.0f};
+float angle_bias[2] = {0.0f, 0.0f};
+
+float imu_data[5];
+
+constexpr float bias_gain = 0.00001;     // averaging decay rate - should be very low.
 
 
 
@@ -36,14 +48,21 @@ WiFiServer server(23);
 boolean alreadyConnected = false; // whether or not the client was connected previously
 
 void setup() {
+  // while (true);
+  
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  
+
+    delay(5000);
+      Serial.println("starting setup");
+
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
-    while (true);
+    while (true)
+    
+    ;
   }
 
   String fv = WiFi.firmwareVersion();
@@ -65,26 +84,63 @@ void setup() {
 
 
   }
-  if (!icm.begin_I2C()) {
+if (!icm.begin_I2C()) {
       Serial.println("ERROR: ICM ");
-      while (true);
+        while (true)
+        {
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+        }
     }
+      Serial.println("I2C setup");
+  
+    
 
-  if (!GPS.begin(GPS_I2C_ADDRESS)) 
+        if (!GPS.begin(GPS_I2C_ADDRESS)) 
   {
     Serial.println("ERROR: GPS");
-        while (true);
+        while (true)
+        {
+digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+        }
   }
 
   
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+
+
   //GPS.sendCommand(PMTK_API_SET_FIX_CTL_100_MILLIHERTZ);
   
 
 
-  // wait a second for connection:
+  icm.setAccelRateDivisor(225);
+  icm.setGyroRateDivisor(225);
 
+
+  icm.enableAccelDLPF(true, ICM20X_ACCEL_FREQ_5_7_HZ);
+  icm.enableGyrolDLPF(true, ICM20X_GYRO_FREQ_5_7_HZ);
+  delay(100);
+
+  icm.getEvent(&accel, &gyro, &temp);
+  float ax = float(accel.acceleration.x);
+  float ay = float(accel.acceleration.y);
+  float az = float(accel.acceleration.z);
+//
+  acc_bias[0] = ax;
+  acc_bias[1] = ay;
+  acc_bias[2] = az;
+  filter.setup( ax,ay,az);     
+
+
+
+  // wait a second for connection:
+Serial.println("wifi setup");
 
   delay(1000);
 
@@ -95,25 +151,39 @@ void setup() {
   server.begin();
   // you're connected now, so print out the status:
   printWifiStatus();
+
+  //WiFi.lowPowerMode();
+  //GPS.standby();
+
 }
 unsigned long previousIMUTime = 0;  
 const unsigned long IMUEventInterval = 200; // 200 milliseconds
 
+bool GPS_ON = true;
 
 //WiFiClient client;
 void loop() {
 
   //Serial.println(WiFi.status());
 
+  bool send_imu = false;
+  unsigned long currentTime = millis();  
+  if (currentTime - previousIMUTime >= IMUEventInterval) 
+  {
+    previousIMUTime = millis();
+    update_imu();
+    send_imu = true;
+  }
+  if (GPS_ON)
+  {
+    char c = GPS.read();
+    if (GPS.newNMEAreceived()) GPS.parse(GPS.lastNMEA());
+  }
   
-
-  char c = GPS.read();
-  if (GPS.newNMEAreceived()) GPS.parse(GPS.lastNMEA());
   // check for a new client:
   WiFiClient client = server.available();
   
    
-  // when the client sends the first byte, say hello:
   if (client) 
   {
     client.flush();
@@ -129,13 +199,8 @@ void loop() {
 
     if (client.status() > 0) 
     {
-
-      unsigned long currentTime = millis();  
-      if (currentTime - previousIMUTime >= IMUEventInterval) 
-      {
-          previousIMUTime = millis();
+      if (send_imu) 
           output_imu();
-      }
     }
     else
     {
@@ -169,17 +234,45 @@ void loop() {
 }
 
 
+void update_imu()
+{
+  icm.getEvent(&accel, &gyro, &temp);
+  float gx = float(gyro.gyro.x);
+  float gy = float(gyro.gyro.y); 
+  float gz = float(gyro.gyro.z); 
+  float ax = float(accel.acceleration.x);
+  float ay = float(accel.acceleration.y);
+  float az = float(accel.acceleration.z);
+
+  filter.update(gx, gy, gz, ax, ay, az);
+
+  float pitch = float(filter.pitch());
+  float roll = float(filter.roll());
+
+
+  float v[3] = { ax, ay, az };
+
+  filter.projectVector( true, v );
+
+  acc_bias[0] = (1.0-bias_gain)*acc_bias[0] + bias_gain*v[0];
+  acc_bias[1] = (1.0-bias_gain)*acc_bias[1] + bias_gain*v[1];
+  acc_bias[2] = (1.0-bias_gain)*acc_bias[2] + bias_gain*v[2];
+
+  angle_bias[0] = (1.0-bias_gain)*angle_bias[0] + bias_gain*pitch;
+  angle_bias[1] = (1.0-bias_gain)*angle_bias[1] + bias_gain*roll;
+
+
+  imu_data[0] = v[0]-acc_bias[0];
+  imu_data[1] = v[1]-acc_bias[1];
+  imu_data[2] = v[2]-acc_bias[2];
+  imu_data[4] = pitch-angle_bias[0];
+  imu_data[5] = roll-angle_bias[0];
+
+}
 void output_imu()
 {
 
-    icm.getEvent(&accel, &gyro, &temp);
-    float gx = float(gyro.gyro.x);
-    float gy = float(gyro.gyro.y); 
-    float gz = float(gyro.gyro.z); 
-    float ax = float(accel.acceleration.x);
-    float ay = float(accel.acceleration.y);
-    float az = float(accel.acceleration.z);
-
+    
 
     
     if (GPS.hour < 10) { server.print('0'); }
@@ -199,17 +292,15 @@ void output_imu()
     server.print(",");
     server.print(GPS.longitude);
     server.print(",");
-    server.print(gx);
+    server.print(imu_data[0]);
     server.print(",");
-    server.print(gy);
+    server.print(imu_data[1]);
     server.print(",");
-    server.print(gz);
+    server.print(imu_data[2]);
     server.print(",");
-    server.print(ax);
+    server.print(imu_data[3]);
     server.print(",");
-    server.print(ay);
-    server.print(",");
-    server.println(az);
+    server.println(imu_data[4]);
 
 
 }
